1fc915e1fb5b3b74ed3287bc357cc787cef859a8
Denis <varenikovda@gmail.com>
2022-01-30 17:55:21 +0300

    Commit the whole project

--- colorSetting/ViewController.swift
+++ colorSetting/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
+    @IBOutlet var sliderRead: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelRed.text = String(format: "%.2f", sliderRead.value)
+        labelGreen.text = String(format: "%.2f", sliderGreen.value)
+        labelBlue.text = String(format: "%.2f", sliderBlue.value)
     }
 
-
+    
+    @IBAction func sliderRedAction() {
+        labelRed.text = String(format: "%.2f", sliderRead.value)
+    }
+    
+    @IBAction func sliderGreenAction() {
+        labelGreen.text = String(format: "%.2f", sliderGreen.value)
+    }
+    
+    @IBAction func sliderBlueAction() {
+        labelBlue.text = String(format: "%.2f", sliderBlue.value)
+    }
+    
 }
 
