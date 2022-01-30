3d7ca06b50623f6a629fb661e5d3110c3081461b
Denis <varenikovda@gmail.com>
2022-01-30 18:44:58 +0300

    Update file ViewController

--- colorSetting/ViewController.swift
+++ colorSetting/ViewController.swift
@@ -17,23 +17,49 @@ class ViewController: UIViewController {
     @IBOutlet var sliderGreen: UISlider!
     @IBOutlet var sliderBlue: UISlider!
     
+    @IBOutlet var colorWindow: UIView!
+    
+    var redColorValue: CGFloat!
+    var greenColorValue: CGFloat!
+    var blueColorValue: CGFloat!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redColorValue = CGFloat(sliderRead.value)
+        greenColorValue = CGFloat(sliderGreen.value)
+        blueColorValue = CGFloat(sliderBlue.value)
+        
         labelRed.text = String(format: "%.2f", sliderRead.value)
         labelGreen.text = String(format: "%.2f", sliderGreen.value)
         labelBlue.text = String(format: "%.2f", sliderBlue.value)
+        
+        colorWindow.layer.cornerRadius = 10
+        colorWindow.backgroundColor = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
     }
 
     
     @IBAction func sliderRedAction() {
+        redColorValue = CGFloat(sliderRead.value)
+        
+        colorWindow.backgroundColor = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
+        
         labelRed.text = String(format: "%.2f", sliderRead.value)
     }
     
     @IBAction func sliderGreenAction() {
+        greenColorValue = CGFloat(sliderGreen.value)
+        
+        colorWindow.backgroundColor = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
+        
         labelGreen.text = String(format: "%.2f", sliderGreen.value)
     }
     
     @IBAction func sliderBlueAction() {
+        blueColorValue = CGFloat(sliderBlue.value)
+            
+        colorWindow.backgroundColor = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
+        
         labelBlue.text = String(format: "%.2f", sliderBlue.value)
     }
     
